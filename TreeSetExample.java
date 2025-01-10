daf69329deabc6a6d21219490c4ca02ba24b9f5d
sunil dubey <sdubey0201@gmail.com>
2025-01-11 02:25:47 +0530

    treeSet
    Subset method Example

--- TreeSetExample.java
+++ TreeSetExample.java
@@ -1,6 +1,7 @@
 package com.example1;
 
 import java.util.Arrays;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class TreeSetExample {
@@ -11,6 +12,23 @@ public class TreeSetExample {
 //        which will help in easier navigation when searching for records.
         TreeSet<String> student = new TreeSet<>(Arrays.asList("Sunil", "Akash", "Venu", "Dharmendra"));
         System.out.println(student);
+
+//        2. Range-Based Queries (Subsets)
+//        Use case: You need to retrieve
+//        subsets of data within a certain range.
+//                Example:
+//        Storing product prices in a store
+//        and fetching products within a price range:
+        TreeSet<Integer> price = new TreeSet<>();
+        price.add(201);
+        price.add(100);
+        price.add(300);
+        price.add(305);
+        price.add(410);
+        price.add(105);
+        price.add(115);
+        SortedSet<Integer> integers = price.subSet(101, 202);
+        System.out.println(integers);
 
 
     }
